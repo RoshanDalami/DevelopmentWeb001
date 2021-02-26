8891158e2c6edb05cd34c14eb9560fc837a0801f
rsunar <tech.channel.002@gmail.com>
2021-02-26 18:58:41 +0530

    function on javascript

--- JavaScript/string.js
+++ JavaScript/string.js
@@ -3,6 +3,16 @@
 //  var some = 9;
 // document.getElementById("try").innerHTML =  "myVar is " + myVar + " and some is " + some;
 
-var str = " Apple apple APPLE AppLE AppLe aPPLE";
-var txt = str.replace(/([A-Z])\w+/gi,"google");
-document.getElementById("try").innerHTML = txt;
+// var str = " Apple apple APPLE AppLE AppLe aPPLE";
+// var txt = str.replace(/([A-Z])\w+/gi,"google");
+
+function increaseHighScore(currentScore){
+    var newHighScore = 2 * currentScore ;
+    return newHighScore;
+}
+
+console.log(increaseHighScore(20));
+
+
+
+// document.getElementById("try").innerHTML = txt;
