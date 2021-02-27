4ffc7cfbe3c38961d607e930ff0a8fdfdbd1784e
rsunar <tech.channel.002@gmail.com>
2021-02-28 02:25:26 +0530

    script learning

--- JavaScript/string.js
+++ JavaScript/string.js
@@ -19,22 +19,101 @@
 //     )
 // }
 
-var harley = {
-    name : "Street750",
-    color : "black",
-    getBudgetForCustomStuff : function (){
-        return("You need to lower your Budget",
-        console.log(this.name)
-        )
-    },
-    shoutOuts :[
-        {
-            kevin : "A cool black monster with 750cc"
-        },
-        {sam : "Heavy bike with full power "}
-    ]
-};
+// var harley = {
+//     name : "Street750",
+//     color : "black",
+//     getBudgetForCustomStuff : function (){
+//         return("You need to lower your Budget",
+//         console.log(this.name)
+//         )
+//     },
+//     shoutOuts :[
+//         {
+//             kevin : "A cool black monster with 750cc"
+//         },
+//         {sam : "Heavy bike with full power "}
+//     ]
+// };
+// var walk = function(){
+//     return (" He is able to walk")
+// }
+// var SuperHero = function(){
+    
+//     var power,villian;
+// }
+// SuperHero.prototype.walk = walk;
+// peterParker = new superHero ;
+// peterParker.power = "AirSwing";
+// peterParker.villian = "Green Goblin";
+// peterParker.walk();
 
-harley.getBudgetForCustomStuff(8000)
+// console.log(peterParker.walk())
+// clark = new superHero ;
+// clark.power = "fly";
+// clark.villian = "Lex Luther ";
+// console.log(clark.villian)
+var ups1 = 1;
+// closure function
+var counter = (function()
+    {
+        var addOne = 0;
+        return function(){
+            return addOne += 1;
+        }
+    }
+)();
+console.log(counter())
+//closure ends here
 
-document.getElementById("try").innerHTML = harley.shoutOuts;
+
+
+// var exercise = { benchPress : "4 sets ", squats :"2 sets"}
+// doSomething.call(exercise, exercise.squats);
+// doSomething.apply(exercise, ['crunches']);
+
+
+
+
+// var newList = document.createElement("li");
+// newList.innerHTML = "I was added by JS";
+// document.getElementById("change").appendChild(newList);
+
+
+// document.getElementById("yrr").setAttribute(attribute , value) = ;
+// document.getElementById("yrr").write(text)
+
+function myFunction(){
+    var x = document.forms["myForm"];
+    var text = "";
+    var i;
+    for(i = 0; i < x.lenght ; i++)
+    {
+        text += x.elements[i].value + "<br>";
+
+    }
+    document.getElementsById("getValue").innerHTML = text;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var multiplier = function(){
+//     var result = 1;
+//     for (var i = arguments.length -1 ; i>=0 ; i--)
+//     {
+//         result *= arguments[i];
+//     }
+//      return result;
+// }
+// console.log(multiplier(1,2))
+ document.getElementById("try").innerHTML = "changed by script";
