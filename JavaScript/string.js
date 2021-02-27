91374f84e4a612a8512ef6bec77ba38f6c633ab7
rsunar <tech.channel.002@gmail.com>
2021-02-27 09:10:25 +0530

    day third

--- JavaScript/string.js
+++ JavaScript/string.js
@@ -6,20 +6,35 @@
 // var str = " Apple apple APPLE AppLE AppLe aPPLE";
 // var txt = str.replace(/([A-Z])\w+/gi,"google");
 
-function increaseHighScore(currentScore){
-    var newHighScore = 2 * currentScore ;
-    return newHighScore;
-}
+// function increaseHighScore(currentScore){
+//     var newHighScore = 2 * currentScore ;
+//     return newHighScore;
+// }
 
-console.log(increaseHighScore(20));
+// console.log(increaseHighScore(20));
 
- var newHighScore = function (currentScore){
-    var newHighScore = 2 * currentScore ;
-    return newHighScore;
-}
+//  var newHighScore = function (currentScore){
+//     return (
+//          console.log(arguments)
+//     )
+// }
 
+var harley = {
+    name : "Street750",
+    color : "black",
+    getBudgetForCustomStuff : function (){
+        return("You need to lower your Budget",
+        console.log(this.name)
+        )
+    },
+    shoutOuts :[
+        {
+            kevin : "A cool black monster with 750cc"
+        },
+        {sam : "Heavy bike with full power "}
+    ]
+};
 
+harley.getBudgetForCustomStuff(8000)
 
-
-
-document.getElementById("try").innerHTML = newHighScore(10);
+document.getElementById("try").innerHTML = harley.shoutOuts;
